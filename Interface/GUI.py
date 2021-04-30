078744b57968b350fea7adf1be9bc3f8a7088910
Shashvat Gupta <shashvat6264@gmail.com>
2021-04-30 16:28:10 +0530

    Integrated Difficulty Level Selection

--- Interface/GUI.py
+++ Interface/GUI.py
@@ -1,11 +1,17 @@
 from tkinter import *
 from Interface.Board import *
+from Interface.Levels import *
 
 class GUI:
     def __init__(self):
         self.root = Tk()
+        self.rows = 10
+        self.cols = 10
+        self.difficultyBtn = SelectDifficulty(self.root, self.rows, self.cols)
+        self.difficultyBtn.grid(row = 0, column = 0)
         self.boardFrame = Board(self.root)
-        self.boardFrame.grid(row=0, column=0)
+        self.boardFrame.grid(row=1, column=1, columnspan=10)
+        
     
     def run(self):
         self.root.mainloop()
